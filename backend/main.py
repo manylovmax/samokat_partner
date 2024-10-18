1fb290d70f3c3c5d136ba33d629e22e3113389cf
manylovmax <maximalist000@gmail.com>
2024-10-18 21:37:13 +0300

    переименовал server в backend, заменил django на fastapi, набросал код для отправки email

---
+++ backend/main.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI, Response, status
+from pydantic import BaseModel
+
+# Import smtplib for the actual sending function
+import smtplib, ssl
+
+
+app = FastAPI()
+
+class FormFields(BaseModel):
+    name: str
+    city: str
+    phone_number: str
+    sex: str
+    age: int
+    transport: str
+
+def send_smtp_mail(form_fields):
+    port = 465  # For SSL
+    smtp_server = "smtp.gmail.com"
+    sender_email = "my@gmail.com"  # Enter your address
+    receiver_email = "your@gmail.com"  # Enter receiver address
+    password = "password"
+    message = """\
+    Subject: Анкета соискателя samokat
+
+    ФИО: {name}
+    Город: {city}
+    Номер телефона: {phone_number}
+    Пол: {sex}
+    Возраст: {age}
+    Транспорт: {transport}""".format(**form_fields)
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+
+
+
+@app.post("/send-email")
+async def send_email(form_fields: FormFields):
+    send_smtp_mail(form_fields)
+    
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
