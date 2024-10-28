b218e035920d163cac7bd9a4f59fca34daa3eb84
manylovmax <maximalist000@gmail.com>
2024-10-28 19:56:43 +0300

    изменил роут к политике конфиденциальности

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -13,7 +13,7 @@ const router = createBrowserRouter([
     element: <HomePage />,
   },
   {
-    path: "/confidentiality-policy",
+    path: "/privacy",
     element: <ConfidentialityPolicy />,
   },
 ]);
